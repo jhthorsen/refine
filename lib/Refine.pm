02dc85cdc66ef4ebfcf35c6328a83734c0f9a22e
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-07-04 11:15:15 +0200

    Started on Refine.pm

---
+++ lib/Refine.pm
@@ -0,0 +1,77 @@
+package Refine;
+
+=head1 NAME
+
+Refine - Refine a class to your desire
+
+=head1 VERSION
+
+0.01
+
+=head1 DESCRIPTION
+
+L<Refine> is a module that export C<$_refine> which can be used to add
+methods object instances.
+
+=head1 SYNOPSIS
+
+  use Refine;
+  use Data::Dumper ();
+
+  my $obj = Some::Class->new;
+
+  # add the dump() method to the $obj instance
+  $obj->$_refine(
+    dump => sub { Data::Dumper->new([$_[0])->Terse(1)->SortKeys(1)->Dump },
+  );
+
+=cut
+
+use strict;
+use warnings;
+use Carp ();
+use base 'Exporter';
+
+our $VERSION = '0.01';
+our @EXPORT = '$_refine';
+
+my %PRIVATE2PUBLIC;
+
+our $_refine = sub {
+  my ($self, %patch) = @_;
+  my $class = ref $self || $self;
+  my $private_name = join ':', $class, map { $_, $patch{$_} } sort keys %patch;
+  my $refined_class = $PRIVATE2PUBLIC{$private_name};
+
+  unless ($refined_class) {
+    my $i = 0;
+    my $public_name = substr +("$class\::__WITH__::" .join '::', sort keys %patch), 0, 180;
+    do {
+      $refined_class = "$public_name\::_$i";
+      $i++;
+    } while ($refined_class->can('new'));
+    $PRIVATE2PUBLIC{$private_name} = $refined_class;
+    eval "package $refined_class;use base '$class';1" or Carp::confess($@);
+    no strict 'refs';
+    *{"$refined_class\::$_"} = $patch{$_} for keys %patch;
+  }
+
+  no strict 'refs';
+  bless $self, $refined_class;
+  $self;
+};
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014, Jan Henning Thorsen
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
+=head1 AUTHOR
+
+Jan Henning Thorsen - C<jhthorsen@cpan.org>
+
+=cut
+
+1;
